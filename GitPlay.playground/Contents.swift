10bdb712a7fc262ca7fa125f007a792b7bc20026
Bryan Bryce <Bryan.Bryce@gmail.com>
2015-10-15 13:56:17 -0600

    new stuff

---
+++ GitPlay.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+func fun(times: Int) {
+    for _ in 0..<times {
+        print("fun")
+    }
+}
+
+fun(10)
